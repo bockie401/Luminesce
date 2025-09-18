10326980cb7de305e9413f954cb930eabdbfdeb7
bockie401 <bockie.401@gmail.com>
2025-09-18 18:47:22 -0400

    Initial commit

---
+++ src/main/java/net/bockie/luminesce/LuminesceClient.java
@@ -0,0 +1,10 @@
+package net.bockie.luminesce;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class LuminesceClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/net/bockie/luminesce/LuminesceDataGenerator.java
@@ -0,0 +1,11 @@
+package net.bockie.luminesce;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class LuminesceDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
